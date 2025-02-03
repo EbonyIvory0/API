7f33d8e9d95fe9063c43fc8d1b6f072efe3fc9ce
Nikita <pechka959@gmail.com>
2025-02-03 15:52:47 +0300

    reviev code and add payloads.py

--- test_start.py
+++ test_start.py
@@ -1,25 +1,26 @@
-import pytest
-import requests
-from test_api import store, pet, user
+from test_api import Store, Pet, User
 
-store_api = store
-pet_api = pet
-user_api = user
+store_api = Store()
+pet_api = Pet()
+user_api = User()
+
 
 def test_01_store_API():
-    store_api.GET()
-    store_api.POST()
-    store_api.GET_ORDER_ID()
-    store_api.DELETE_ORDER_ID()
+    store_api.get()
+    store_api.post()
+    store_api.get_order_id()
+    store_api.delete_order_id()
+
 
 def test_02_pet_API():
-    pet_api.POST_pet()
-    pet_api.GET_pet_by_STATUS()
-    pet_api.PUT_pet()
-    pet_api.DELETE_pet()
+    pet_api.post_pet()
+    pet_api.get_pet_by_status()
+    pet_api.put_pet()
+    pet_api.delete_pet()
+
 
 def test_03_user_API():
-    user_api.POST_user()
-    user_api.GET_user()
-    user_api.PUT_user()
-    user_api.DELETE_user()
+    user_api.post_user()
+    user_api.get_user()
+    user_api.put_user()
+    user_api.delete_user()
